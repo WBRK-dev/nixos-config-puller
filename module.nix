b920a83da141359a588dfe23ac21cff7b7440a8d
Koert Weber <koertweber@wbrk.dev>
2026-03-13 11:09:55 +0100

    feat: added module.nix

---
+++ module.nix
@@ -0,0 +1,50 @@
+{ lib, pkgs, config, ... }:
+
+let
+  cfg = config.services.nixos-config-puller;
+in
+{
+  options.services.nixos-config-puller = {
+    enable = lib.mkEnableOption "nixos-config-puller";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.callPackage ./default.nix {};
+      description = "The nixos-config-puller package to use.";
+    };
+
+    user = lib.mkOption {
+      type = lib.types.str;
+      default = "";
+      description = "User account that should run nixos-config-puller at login. Empty disables autostart service.";
+    };
+
+    autoStart = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Whether to autostart nixos-config-puller for the configured user.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [ cfg.package ];
+
+    systemd.user.services.nixos-config-puller = lib.mkIf (cfg.autoStart && cfg.user != "") {
+      description = "NixOS Config Puller";
+
+      wantedBy = [ "default.target" ];
+      after = [ "graphical-session.target" "network-online.target" ];
+      wants = [ "network-online.target" ];
+
+      serviceConfig = {
+        ExecStart = "${cfg.package}/bin/nixos-config-puller";
+        Restart = "on-failure";
+        RestartSec = 5;
+      };
+    };
+
+    users.users.${cfg.user} = lib.mkIf (cfg.autoStart && cfg.user != "") {
+      isNormalUser = true;
+    };
+  };
+}
